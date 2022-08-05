d2724f7a9f212a2b7d85dbbc7d1b9628ecbcb48e
JeongHyeSoo <nailonly@naver.com>
2022-08-05 22:57:57 +0900

    두 정수 사이의 합_1

---
+++ src/main/java/Lv_1/두_정수_사이의_합.java
@@ -0,0 +1,18 @@
+package Lv_1;
+
+public class 두_정수_사이의_합 {
+    public long solution(int a, int b) {
+        long answer = 0;
+        if (a <= b) {
+            for (int i = a; i <= b; i++) {
+                answer = answer + i;
+            }
+        } else {
+            for (int i = b; i <= a; i++) {
+                answer = answer + i;
+            }
+        }
+
+        return answer;
+    }
+}
